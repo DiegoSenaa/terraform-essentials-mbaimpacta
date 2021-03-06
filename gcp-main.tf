00db72e23a21cf0f1b4e5e49c221ec4c0528b501
Diego Sena <diego.senaa@gmail.com>
2021-03-06 11:17:35 -0300

    Refactory projeto cloud

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,16 +1,11 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-}
+#### RECURSOS NO GCP #####
 
+# Cria uma VM no Google Cloud
 resource "google_compute_instance" "firstvm" {
   name         = "website"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  tags    = ["website", "impacta"]
+  zone         = var.gcp_zone
+  tags         = ["website", "impacta"]
 
   # Defini a Imagem da VM
   boot_disk {
@@ -24,12 +19,7 @@ resource "google_compute_instance" "firstvm" {
     network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
 
     access_config {
-      // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
     }
+    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
   }
-}
-
-# Retorna o IP da VM criada
-output "ip" {
-  value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
 }
--- provider.tf
+++ provider.tf
@@ -1,14 +1,25 @@
+#### SETUP GOOGLE ####
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.58.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.49.0"
+    }
+  }
+}
+
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  version = "3.5.0"
-  project     = "diegobastos-cloudautomation"
-  region  = "us-central1"
-  zone    = "us-central1-b"
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
 }
 
 # # Configura o Provider Microsoft Azure
 # provider "azurerm" {
-#   version         = "=2.20.0"
-
 #   features {}
 # }
