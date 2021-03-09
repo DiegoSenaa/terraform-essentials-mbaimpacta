190eb35c5dda7cedf7745700992312ef70931400
Diego Sena <diego.senaa@gmail.com>
2021-03-09 20:19:30 -0300

    Adição cloudflare

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "gcp_zone" {
   default     = "us-central1-c"
 }
 
-# ### AZURE
+### AZURE
 
 variable "rg_location" {
   type        = string
@@ -27,4 +27,30 @@ variable "rg_location" {
 variable "azurevm_admin_pass" {
   type        = string
   description = "Senha Usuario Admin do Linux"
+}
+
+### CLOUDFLARE
+
+#Cloudflare email
+variable "cloudflare_email" {
+  type = string
+  description = "Cloudflare Email Address"
+}
+
+#Cloudflare API chave
+variable "cloudflare_api_key" {
+  type = string
+  description = "Cloudflare API Key"
+}
+
+#Cloudflare zone id
+variable "cloudflare_zone_id" {
+  type = string
+  description = "Cloudflare Zone ID"
+}
+
+#Cloudflare Record Name
+variable "cloudflare_record" {
+  type = string
+  description = "Cloudflare Record Name"
 }
